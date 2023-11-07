2310f3751390140e5c3bf072c58b1eddf10aa60d
Adarsh Bhardwaj <adarshbhardwaj285@gmail.com>
2023-11-07 19:31:34 +0530

    User Dashboard Completed

---
+++ include/header.php
@@ -0,0 +1,33 @@
+<nav>
+    <div class="logo-container" onclick="window.location.href='../'">
+        <img src="../assets/images/logo/logo.png" alt="web-log" class="logo">
+        <h1>ReadRipple</h1>
+    </div>
+    <div class="header-menu">
+        <div class="menu">
+            <ul>
+                <!-- <li><a href="#">Home</a></li> -->
+                <li><a href="../book-exchange/">Book Exchange</a></li>
+                <li class="dropdown-container">
+                    <div class="dropdown">
+                        <a href="#">Other Options <i class='fas fa-angle-down'></i></a>
+                    </div>
+                    <div class="dropdown-items">
+                        <div class="dropdown-links"><a href="#">Giveaway Book <i class="fa fa-arrow-right"></i></a></div>
+                        <div class="dropdown-links"><a href="#">Take Book <i class="fa fa-arrow-right"></i></a></div>
+                        <div class="dropdown-links"><a href="#">Borrow Book <i class="fa fa-arrow-right"></i></a></div>
+                    </div>
+                </li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+        </div>
+        
+        <div class="btn-group">
+            <a href="../login/" class="link">Login</a>
+            <a href="../sign-up/" class="btn">Sign Up <i class="fa fa-arrow-right"></i> </a>
+        </div>
+
+    </div>
+
+
+</nav>
